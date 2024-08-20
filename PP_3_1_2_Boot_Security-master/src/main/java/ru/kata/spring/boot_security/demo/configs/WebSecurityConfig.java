2f87a8453a773e1b4fb456584330bb820f48fadd
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-08-20 12:44:06 +0300

    Add files via upload

---
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+}
---
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/init/AddUserAndRoleInTable.java
@@ -0,0 +1,31 @@
+package ru.kata.spring.boot_security.demo.init;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.entity.Role;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+
+@Component
+public class AddUserAndRoleInTable {
+
+    private final RoleRepository roleRepos;
+    private final UserService userService;
+
+    @Autowired
+    public AddUserAndRoleInTable(RoleRepository roleRepos, UserService userService) {
+        this.roleRepos = roleRepos;
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    private void init() {
+        roleRepos.save(new Role(1L, "ROLE_ADMIN"));
+        roleRepos.save(new Role(2L, "ROLE_USER"));
+        userService.saveUser(new User("Igor", "igor@gmail.com", "12345678", roleRepos.findAll()));
+        userService.saveUser(new User("Oleg","Oleg@mail.ru","1234",roleRepos.findAll()));
+    }
+}
