ff2420aaa3d9faff6a98bc40d0579a0b3e238033
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-08-20 04:35:19 +0300

    Add files via upload

---
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String getUserProfile(Model model, Long id) {
+        model.addAttribute("user", userService.findById(id));
+        return "user";
+    }
+}
